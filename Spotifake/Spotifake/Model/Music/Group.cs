354393828542480becfb78cbd496b3957037a9b0
nefaryan.dev@hotmail.com <nefaryan.dev@hotmail.com>
2023-11-24 20:38:38 +0100

    #

--- Spotifake/Spotifake/Model/Music/Group.cs
+++ Spotifake/Spotifake/Model/Music/Group.cs
@@ -15,7 +15,7 @@ namespace Spotifake.Model.Music
         List<Album> _albums;
         List<Song> _song;
         string _bio;
-        private string groupName;
+      
 
         public Group() { }
         public Group(int id, string name, string bio)
@@ -23,21 +23,19 @@ namespace Spotifake.Model.Music
             _id = id;
             _bio = bio;
             _gruopName = name;
-           
+            _artists = new List<Artist>();
             _albums = new List<Album>();
             _song = new List<Song>();
         }
 
-        public Group(string groupName)
-        {
-            this.groupName = groupName;
-        }
+ 
 
         public string GruopName { get => _gruopName; set => _gruopName = value; }
         public string Bio { get => _bio; set => _bio = value; }
-        internal List<Song> Song { get => _song; set => _song = value; }
-        internal List<Album> Albums { get => _albums; set => _albums = value; }
+        public List<Song> Song { get => _song; set => _song = value; }
+        public List<Album> Albums { get => _albums; set => _albums = value; }
         public int Id { get => _id; set => _id = value; }
+        public List<Artist> Artists { get => _artists; set => _artists = value; }
 
         public void AddArtist(Artist artist)
         {
