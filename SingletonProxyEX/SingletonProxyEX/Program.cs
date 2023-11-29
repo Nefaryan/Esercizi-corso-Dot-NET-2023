c2655ad44dabde8daf78f5092e3f3aa768bfeb36
nefaryan.dev@hotmail.com <nefaryan.dev@hotmail.com>
2023-11-29 09:13:36 +0100

    Singleton EX #

---
+++ SingletonProxyEX/SingletonProxyEX/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace SingletonProxyEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NInstance = 8;// numero di istanze al proxy
+            int CCall = 4;// nUmero di chiamate per istanze
+
+            for(int i = 0; i < NInstance; i++)
+            {
+                ServerProxy proxy = ServerProxy.Instance;
+
+                for(int j = 0; j < CCall; j++)
+                {
+                    string IndirizzoIP = proxy.ServerRequest();
+                    Console.WriteLine($"Proxy {i + 1}, Chiamata {j + 1}: {IndirizzoIP}");
+
+                }
+            
+            }
+        }
+    }
+}
---
+++ SingletonProxyEX/SingletonProxyEX/ServerProxy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonProxyEX
+{
+    //Classe singleton per il proxy
+    public class ServerProxy
+    {
+
+        private static ServerProxy instance;
+        private List<string> serversIP;
+
+        private ServerProxy()   
+        {
+            serversIP = new List<string>()
+            {
+                IPGenerator(),
+                IPGenerator(),
+                IPGenerator(),
+                IPGenerator()
+            };
+        }
+
+        private string IPGenerator()
+        {
+            Random random = new Random();
+            return $"{random.Next(256)}.{random.Next(256)}.{random.Next(256)}.{random.Next(256)}";
+        }
+
+        public static ServerProxy Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new ServerProxy();
+                }
+                return instance;
+            }
+        }
+
+        public string ServerRequest()
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(serversIP.Count);
+            return serversIP[randomIndex];
+        }
+    }
+
+
+    
+}
+
