c312f203ebf12b5d83fc1bab1053eab0a032c563
nefaryan.dev@hotmail.com <nefaryan.dev@hotmail.com>
2023-12-04 12:53:44 +0100

    # set up classes for movie

---
+++ SpotifakeV2/SpotifakeData/Repository/MovieRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using SpotifakeData.DataContext;
+using SpotifakeData.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifakeData.Repository
+{
+    public class MovieRepository
+    {
+        private readonly string _folderPath = @"C:\Users\giuse\Desktop\SpotiFake\Movie";
+        private readonly ILogger<MovieRepository> _logger;
+        private DBContext _dbContext;
+
+        public MovieRepository(ILogger<MovieRepository> logger)
+        {
+            _logger = logger;
+            _dbContext = new DBContext(_folderPath);
+        }
+
+        public List<MovieRepository> GetAll()
+        {
+            return null;
+        }
+        public Movie GetById(int id)
+        {
+            return null;
+        }
+
+        public Movie GetByName(string name)
+        {
+            return null;
+        }
+
+        public void Added(Movie movie)
+        {
+           //TODO
+        }
+    }
+}
