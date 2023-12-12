d43d0079abcb6d252a488c5ce95d507aded677f6
nefaryan.dev@hotmail.com <nefaryan.dev@hotmail.com>
2023-12-12 12:47:19 +0100

    #Create jsonDataContext whit CRUD OPERATION

--- OrderProject/Data/Context/DataContex.cs
+++ OrderProject/Data/Context/DataContex.cs
@@ -1,9 +1,11 @@
 ﻿using Data.Interfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Data.Context
@@ -24,27 +26,63 @@ namespace Data.Context
 
         public void AddItem(T item)
         {
-            throw new NotImplementedException();
+            List<T> items = GetAll();
+            items.Add(item);
+            SavaData(items);
         }
 
         public string Delete(int id)
         {
-            throw new NotImplementedException();
+            List<T> items = GetAll();
+            T itemToDelete = items.FirstOrDefault(i=>GetId(i) == id);
+            if(itemToDelete != null)
+            {
+                items.Remove(itemToDelete);
+                SavaData(items);
+                return "Elemento eliminato";
+            }
+            else
+            {
+                return "Elemento non trovato";
+            }
         }
 
         public List<T> GetAll()
         {
-            throw new NotImplementedException();
+            string data = File.ReadAllText(_filePath);
+            List<T> list =JsonConvert.DeserializeObject<List<T>>(data);
+            return list ?? new List<T>();
         }
 
         public T GetById(int id)
         {
-            throw new NotImplementedException();
+            List<T> items = GetAll();
+            return items.FirstOrDefault(i=>GetId(i) == id);
         }
 
         public void Update(int id, T item)
         {
-            throw new NotImplementedException();
+            List<T> items = GetAll();
+            T itemToUpdate = items.FirstOrDefault(i =>GetId(i) == id);
+            if(itemToUpdate != null)
+            {
+                items.Remove(itemToUpdate);
+                items.Add(item);
+                SavaData(items);
+            }
         }
+
+        private void SavaData(List<T> item) 
+        { 
+            string data = JsonConvert.SerializeObject(item, Formatting.Indented);
+            File.WriteAllText(_filePath, data);
+        }
+
+        private int GetId(T item)
+        {
+            var idProperty = typeof(T).GetProperty("Id");
+            return (int)idProperty.GetValue(item);
+        }
+
     }
 }
