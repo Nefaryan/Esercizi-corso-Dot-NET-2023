72f87e7eb9ac7f06896eab89fa19c239ad8c16e9
nefaryan.dev@hotmail.com <nefaryan.dev@hotmail.com>
2023-12-18 12:23:03 +0100

    #

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -5,10 +5,13 @@ namespace Delegate
     internal class Program
     {
         delegate int DelegateSumm(int x, int y);
+        delegate bool ProductCheck(int prod, int thold);
 
         static void Main(string[] args)
         {
-            
+            //Esercizio 1
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("------------------------Esercizio-1-------------------");
             DelegateSumm somma = Summ;
  
             Func<DelegateSumm, int, int, int> dell2 = Operation;
@@ -16,6 +19,25 @@ namespace Delegate
             int result = dell2(somma, 2, 3);
             
             FinalResult(result);
+
+            // Esercizio 2
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("------------------------Esercizio-2-------------------");
+
+            Func<int, int, int> prodCalculator = (a, b) => a * b;
+            Predicate<int> productTHoldCheck = res => res > 9;
+            Action<bool> positiveCallBack = res =>
+            {
+                if (res)
+                {
+                    Console.WriteLine("Il prodotto è maggiore");
+                }
+            };
+
+            int prodResult = prodCalculator(7, 4);
+            bool isGraterThenValue = productTHoldCheck(prodResult);
+            positiveCallBack(isGraterThenValue);
+
         }
 
         private static int Summ(int x, int y)
