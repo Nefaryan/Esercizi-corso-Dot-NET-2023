67ae80024414f8f545cf6c6b74ad4382c821bbb4
nefaryan.dev@hotmail.com <nefaryan.dev@hotmail.com>
2023-12-22 10:01:52 +0100

    Possibile cambiamento della struttura del db

---
+++ SpotifakeAPI/SpotifakeAPI/Models/Radio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SpotifakeAPI.Models
+{
+    public partial class Radio
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SpotifakeAPI/SpotifakeAPI/Models/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SpotifakeAPI.Models
+{
+    public partial class Song
+    {
+        public int Id { get; set; }
+        public int? Rating { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int? Duration { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
